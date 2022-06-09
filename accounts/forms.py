10f30b71e7f13a4eece0eb685c6ed54807c3365c
XXmmRR <Xorex50@protonmail.com>
2022-06-09 11:00:55 +0300

    Русские названия полей

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,14 @@
 # accounts/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput())
+    email = forms.EmailField(label='Email', widget=forms.EmailInput())
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput())
 
     class Meta(UserCreationForm):
         model = CustomUser
@@ -19,3 +23,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'full_name', 'phone', 'address',)
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput())
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, LoginUserForm
 from django.urls import reverse_lazy
 from django.views import generic
 from catalog.models import PartList
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class SignUpView(generic.CreateView):
@@ -9,3 +11,8 @@ class SignUpView(generic.CreateView):
     extra_context = {PartList.objects.all(): 'parts'}
     success_url = reverse_lazy('login')
     template_name = 'registration/registration.html'
+
+
+class LoginView(LoginView):
+    form_class = LoginUserForm
+    template_name = 'registration/login.html'
